9fb760ed2ebb2ff7eefbeeda219ebbabba19ea8c
MetelevNikita <Kyle.B@mail.ru>
2025-01-28 09:41:00 +0500

    fix proble to create logger

--- src/logger.js
+++ src/logger.js
@@ -1,16 +1,17 @@
 import winston from 'winston';
 
 
-export const logger = new winston.Logger({
+const logger = new winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.json(),
     winston.format.timestamp(),
   ),
   defaultMeta: { service: 'LOG' },
-
   transports: [
     new winston.transports.File({ filename: './src/logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: './src/logs/combined.log' }),
   ]
 })
+
+export default logger;
