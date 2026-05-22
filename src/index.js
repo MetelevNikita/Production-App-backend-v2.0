a35c1f227b115d9ed29cdb8e8c36e10d31c4a00e
Nikita Metelev <Kyle.B@mail.ru>
2026-05-22 14:25:32 +0500

    fix socks proxy 2.0

--- src/index.js
+++ src/index.js
@@ -153,7 +153,7 @@ const bot = new TelegramBot(TOKEN, {
     polling: true,
     request: {
       agent: socksAgent ?? undefined,
-      timeout: 3000,          // сетевой таймаут запроса
+      timeout: 60000,          // сетевой таймаут запроса
     }
 });
 
